3cb3429aef4a406837c0efb893405abc6332c09a
inkyssj <damus203@gmail.com>
2025-05-14 10:11:24 -0300

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -2,7 +2,7 @@ module.exports = () => {
 	name: 'Menú',
 	command: ['menu', 'menus'],
 	prefix: true,
-	models: "%prefix%command",
+	models: '%prefix%command',
 	desactive: false,
 	runCode: async(m, { sock }) => {
 		await m.reply('Menu aun no disponible!')
