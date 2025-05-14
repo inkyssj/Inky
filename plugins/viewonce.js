74776267567bdb9eb6db86cca178a2d297f1e72f
inkyssj <damus203@gmail.com>
2025-05-14 11:55:38 -0300

    Update viewonce.js

--- plugins/viewonce.js
+++ plugins/viewonce.js
@@ -5,11 +5,11 @@ module.exports =  {
 	models: '%prefix%command',
 	desactive: false,
 	runCode: async(m, { sock }) => {
-    if (m.quoted) {
-      let buffer = await m.quoted.download()
-      if (m.quoted.msg.mimetype) m.quoted.msg.mimetype.includes('video') ? (await m.replyVid(buffer)) : (await m.replyImg(buffer))
-    } else {
-      m.reply('Responde a un mensaje para ejecutar')
-    }
+		if (m.quoted) {
+			let buffer = await m.quoted.download()
+			if (m.quoted.msg.mimetype) m.quoted.msg.mimetype.includes('video') ? (await m.replyVid(buffer)) : (await m.replyImg(buffer))
+		} else {
+			m.reply('Responde a un mensaje para ejecutar')
+		}
 	}
 }
