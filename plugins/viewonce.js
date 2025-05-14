def1682b28a081668d873b932d0db99663deba4f
inkyssj <damus203@gmail.com>
2025-05-14 11:39:07 -0300

    Update viewonce.js

--- plugins/viewonce.js
+++ plugins/viewonce.js
@@ -7,7 +7,7 @@ module.exports =  {
 	runCode: async(m, { sock }) => {
     if (m.quoted) {
       let buffer = await m.quoted.download()
-      m.quoted.mimetype.includes('video') ? (await m.replyVid(buffer)) : (await m.replyImg(buffer))
+      if (m.quoted.mimetype) m.quoted.mimetype.includes('video') ? (await m.replyVid(buffer)) : (await m.replyImg(buffer))
     } else {
       m.reply('Responde a un mensaje para ejecutar')
     }
