1ba6edd1d306dcc0caeb47ab24617ce4ad60f4f3
inkyssj <damus203@gmail.com>
2025-05-14 12:33:15 -0300

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -5,6 +5,23 @@ module.exports =  {
 	models: '%prefix%command',
 	desactive: false,
 	runCode: async(m, { sock }) => {
-		await m.reply('Menu aun no disponible!')
+		const moment = require('moment-timezone')
+		const argentinaDate = moment().tz('America/Argentina/Buenos_Aires')
+		
+		let tesk = `> *Inky - Bot*
+
+- *Descripción*.
+      *WhatsApp Bot* creado a partir de la *Libreria Baileys*, para automatizar y ayudar al usuario.
+
+- *Informacion*.
+      *Pais*: Argentina
+      *Fecha*: ${argentinaDate.format('DD/MM/YYYY')}
+      *Hora*: ${argentinaDate.format('HH:mm')}hs
+
+- *Comandos*.
+      *Prefijo*: -
+      *Observación*: Comandos en creación.
+`
+		await m.reply(teks)
 	}
 }
