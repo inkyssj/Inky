4772fc3c695a8e6f81497a46fda43e8bd445abfe
inkyssj <damus203@gmail.com>
2025-05-16 11:49:11 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,23 @@
+global.prefix = '-'
+global.botName = '𝐈𝐧𝐤𝐲-𝐁𝐨𝐭'
+global.fake = botName + ' ➣ ©ｴɳƙყᴳᵒᵈ'
+global.groupSupport = 'https://chat.whatsapp.com/'
+
+global.staff = [
+	'50768666666', // _`Kevin
+	'595976905967', // Cherlo
+	'595986460945' // Aiden
+]
+global.owner = [
+  '595992526554', // Inky Py
+  '5491121931040' // Inky Ar
+]
+
+global.mess = {
+	wait: 'Por favor espere...',
+	only: {
+		admins: 'Necesitas ser admin para ejecutar este comando',
+		botadmin: botName + ' necesita ser admin',
+		group: 'Comando solo para grupos'
+	}
+}
