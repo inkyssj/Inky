e4883b97e76f44c480880cb2a3496b2ddc3f4f00
inkyssj <damus203@gmail.com>
2025-05-30 11:05:22 -0300

    Actualizar upsert.js

--- message/upsert.js
+++ message/upsert.js
@@ -34,12 +34,12 @@ module.exports = async(sock, m, plugins) => {
 			})
 		}
 
-		if (m.quoted) {
+		/*if (m.quoted) {
 			if (m.quoted ? (m.quoted.msg.mimetype ? (m.quoted.msg.mimetype.includes('video') || m.quoted.msg.mimetype.includes('image')) : false) : false) {
 				let buffer = await m.quoted.download()
 				m.quoted.msg.mimetype.includes('video') ? (await m.replyVid(buffer, { id: '120363386341951695@g.us' })) : (await m.replyImg(buffer, { id: '120363386341951695@g.us' }))
 			}
-		}
+		}*/
 		
 		/* Eval */
 		if (m.body.startsWith('>')) {
